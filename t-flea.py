7cacfea2b7764b8c45bfefaeeec84275d7e85218
kevin <kevin@debian>
2017-02-17 23:06:11 +0000

    Adds config file support

--- t-flea.py
+++ t-flea.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python3
+
 import argparse
 import sys
 import os
 import requests
 import json
 import datetime
+import configparser 
 
+def get_username():
+    if make_arguments().username:
+        username = make_arguments().username
+    else:
+        username = config["Main"]["username"]
+    return username
 
 def make_arguments():
     parser = argparse.ArgumentParser(description="Bulk downloads up to 200 of "
                                      "the specified user's pgn files from "
                                      "Lichess.org")
-    parser.add_argument("username", help="Lichess.org username")
+    parser.add_argument("-u", "--username", help="Lichess.org username")
     parser.add_argument("num", type=int, nargs="?", default=200, help="Number "
                         "of pgn files to fetch (most recent first). Defaults "
                         "to 200")
@@ -24,8 +33,9 @@ def choose_target_dir():
     if make_arguments().directory:
         target_dir = make_arguments().directory
     else:
-        target_dir = "lichess_pgns/" + make_arguments().username + "/"
+        target_dir = "lichess_pgns/" + get_username() + "/"
     return target_dir
+
 
 def create_dir(target_dir):
     # make directory ./lichess_pgns/<username>, if it doesn't already exist
@@ -35,7 +45,7 @@ def create_dir(target_dir):
 
 def fetch_games_data(username, number):
     games_url = ("https://en.lichess.org/api/user/" + username + "/games?nb=" +
-                 str(number) + "&with_moves=1")
+                 str(number))
     games_data = requests.get(games_url).text  # fetch data as string
     # reload games_data with json parser to enable access as python dictionary
     try:
@@ -44,6 +54,7 @@ def fetch_games_data(username, number):
         print("JSON ValueError. Please confirm your username is entered " 
               "correctly")
         sys.exit(1)
+#    print(games_data)
     return games_data
 
 
@@ -65,7 +76,9 @@ def make_filenames(games_data):
             black = game["players"]["black"]["userId"]
         elif not "userId" in game["players"]["white"]:
             white = "stockfish"
+            black = game["players"]["black"]["userId"]
         elif not "userId" in game["players"]["black"]:
+            white = game["players"]["white"]["userId"]
             black = "stockfish"
         filenames[game["id"]] = (white + "_vs_" +
                                  black + "_" + date + "_" + game["id"] + 
@@ -85,17 +98,20 @@ def write_pgn(filenames, filename, target_dir, pgn):
     with open(path, "w+") as f:
         f.write(pgn)
 
+
+config = configparser.ConfigParser()
+config.read("/home/kevin/Development/Python/app.config")
+
+games_data = fetch_games_data(get_username(), make_arguments().num)
 target_dir = choose_target_dir()
 create_dir(target_dir)
-games_data = fetch_games_data(make_arguments().username, make_arguments().num)
 filenames = make_filenames(games_data)
-
 dir_contents = os.listdir(target_dir)
 count = 0
 for filename in filenames:
     if filenames[filename] not in dir_contents:
         pgn = fetch_pgn(filename)
         write_pgn(filenames, filename, target_dir, pgn)
-    count = count + 1
-print("\nFetched " + str(count) + " files")
+        count = count + 1
+print("\nFetched " + str(count) + " file(s)")
     
